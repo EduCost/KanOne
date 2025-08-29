132ae48e65dfa43800276f4593e1717ba5335917
EduCost <edu.oliveira03c@gmail.com>
2025-08-29 11:54:07 -0300

    Updated Gradle dependencies and removed unused imports

--- app/src/main/java/com/educost/kanone/domain/repository/AttachmentRepository.kt
+++ app/src/main/java/com/educost/kanone/domain/repository/AttachmentRepository.kt
@@ -1,10 +1,8 @@
 package com.educost.kanone.domain.repository
 
-import com.educost.kanone.domain.error.FetchDataError
 import com.educost.kanone.domain.error.GenericError
 import com.educost.kanone.domain.model.Attachment
 import com.educost.kanone.utils.Result
-import kotlinx.coroutines.flow.Flow
 
 interface AttachmentRepository {
 
