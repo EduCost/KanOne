522c5d50690d1166b05229926e3d7d68363a3270
EduCost <edu.oliveira03c@gmail.com>
2025-09-18 11:13:06 -0300

    Removed custom font families to use default system fonts

--- app/src/main/java/com/educost/kanone/presentation/theme/Type.kt
+++ app/src/main/java/com/educost/kanone/presentation/theme/Type.kt
@@ -8,61 +8,55 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.educost.kanone.R
 
-val exoFontFamily = FontFamily(
-    Font(R.font.exo_regular, FontWeight.Normal),
-    Font(R.font.exo_bold, FontWeight.Bold)
-)
+//val exoFontFamily = FontFamily(
+//    Font(R.font.exo_regular, FontWeight.Normal),
+//    Font(R.font.exo_bold, FontWeight.Bold)
+//)
+//
+//val notoSansFontFamily = FontFamily(
+//    Font(R.font.noto_sans_regular, FontWeight.Normal),
+//    Font(R.font.noto_sans_bold, FontWeight.Bold),
+//    Font(R.font.noto_sans_medium, FontWeight.Medium)
+//)
 
-val notoSansFontFamily = FontFamily(
-    Font(R.font.noto_sans_regular, FontWeight.Normal),
-    Font(R.font.noto_sans_bold, FontWeight.Bold),
-    Font(R.font.noto_sans_medium, FontWeight.Medium)
-)
 val Typography = Typography(
     headlineSmall = TextStyle(
-        fontFamily = exoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp
     ),
     titleLarge = TextStyle(
-        fontFamily = exoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     titleMedium = TextStyle(
-        fontFamily = exoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = notoSansFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = notoSansFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.5.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = notoSansFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = notoSansFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
