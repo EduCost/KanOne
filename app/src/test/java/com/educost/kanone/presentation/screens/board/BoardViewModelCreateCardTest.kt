e3b27a755a8034f22fade3dd51ca7089695a38f5
EduCost <edu.oliveira03c@gmail.com>
2025-09-22 15:53:32 -0300

    Created BoardViewModelTest super class and updated create card tests

--- app/src/test/java/com/educost/kanone/presentation/screens/board/BoardViewModelCreateCardTest.kt
+++ app/src/test/java/com/educost/kanone/presentation/screens/board/BoardViewModelCreateCardTest.kt
@@ -1,316 +1,144 @@
 package com.educost.kanone.presentation.screens.board
 
-import app.cash.turbine.test
-import com.educost.kanone.dispatchers.DispatcherProvider
-import com.educost.kanone.dispatchers.TestDispatcherProvider
-import com.educost.kanone.domain.model.Board
-import com.educost.kanone.domain.model.KanbanColumn
 import com.educost.kanone.domain.usecase.CreateCardResult
-import com.educost.kanone.domain.usecase.CreateCardUseCase
-import com.educost.kanone.domain.usecase.ObserveCompleteBoardUseCase
 import com.educost.kanone.presentation.screens.board.state.CardCreationState
 import com.educost.kanone.presentation.screens.board.utils.BoardAppBarType
 import com.educost.kanone.utils.Result
 import com.google.common.truth.Truth.assertThat
 import io.mockk.coEvery
 import io.mockk.coVerify
-import io.mockk.mockk
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
-import org.junit.Before
 import org.junit.Test
 
-@Suppress("UnusedFlow")
 @OptIn(ExperimentalCoroutinesApi::class)
-class BoardViewModelCreateCardTest {
+class BoardViewModelCreateCardTest : BoardViewModelTest() {
 
-    private lateinit var testDispatcher: CoroutineDispatcher
-    private lateinit var dispatcherProvider: DispatcherProvider
-    private lateinit var viewModel: BoardViewModel
+    @Test
+    fun `SHOULD update state WHEN start creating card`() = testBoardViewModelUiState {
+        val columnId = 1L
+        viewModel.onIntent(BoardIntent.StartCreatingCard(columnId, true))
 
-    private lateinit var observeCompleteBoardUseCase: ObserveCompleteBoardUseCase
-    private lateinit var createCardUseCase: CreateCardUseCase
-
-    @Before
-    fun setUp() {
-        testDispatcher = UnconfinedTestDispatcher()
-        dispatcherProvider = TestDispatcherProvider(testDispatcher)
-        observeCompleteBoardUseCase = mockk()
-        createCardUseCase = mockk()
-        viewModel = BoardViewModel(
-            dispatcherProvider = dispatcherProvider,
-            observeCompleteBoardUseCase = observeCompleteBoardUseCase,
-            createColumnUseCase = mockk(),
-            createCardUseCase = createCardUseCase,
-            updateColumnUseCase = mockk(),
-            deleteColumnUseCase = mockk(),
-            restoreColumnUseCase = mockk(),
-            persistBoardPositionsUseCase = mockk(),
-            reorderCardsUseCase = mockk(),
-            updateBoardUseCase = mockk(),
-            deleteBoardUseCase = mockk()
-        )
+        val updatedState = awaitItem()
+        assertThat(updatedState.topBarType).isEqualTo(BoardAppBarType.ADD_CARD)
+        assertThat(updatedState.cardCreationState.columnId).isEqualTo(columnId)
     }
 
     @Test
-    fun `SHOULD update state WHEN start creating card`() {
+    fun `SHOULD reset state WHEN cancel card creation`() = testBoardViewModelUiState {
+        viewModel.onIntent(BoardIntent.StartCreatingCard(0, true))
+        skipItems(1)
 
-        coEvery { observeCompleteBoardUseCase(any()) } returns flowOf(
-            Result.Success(Board(id = 1, name = "test", emptyList()))
-        )
-
-        runTest(testDispatcher) {
-            viewModel.uiState.test {
-                viewModel.onIntent(BoardIntent.ObserveBoard(1))
-                skipItems(3)
-
-                viewModel.onIntent(BoardIntent.StartCreatingCard(0, true))
-                val updatedState = awaitItem()
-                assertThat(updatedState.topBarType).isEqualTo(BoardAppBarType.ADD_CARD)
-                assertThat(updatedState.cardCreationState.columnId).isEqualTo(0)
-
-                cancelAndConsumeRemainingEvents()
-                coVerify { observeCompleteBoardUseCase(any()) }
-            }
-        }
+        viewModel.onIntent(BoardIntent.CancelCardCreation)
+        val updatedState = awaitItem()
+        assertThat(updatedState.topBarType).isEqualTo(BoardAppBarType.DEFAULT)
+        assertThat(updatedState.cardCreationState).isEqualTo(CardCreationState())
     }
 
     @Test
-    fun `SHOULD update state WHEN cancel card creation`() {
+    fun `SHOULD update state WHEN new card title change`() = testBoardViewModelUiState {
 
-        coEvery { observeCompleteBoardUseCase(any()) } returns flowOf(
-            Result.Success(Board(id = 1, name = "test", emptyList()))
-        )
+        viewModel.onIntent(BoardIntent.OnCardTitleChange("new title"))
+        assertThat(awaitItem().cardCreationState.title).isEqualTo("new title")
 
-        runTest(testDispatcher) {
-            viewModel.uiState.test {
-                viewModel.onIntent(BoardIntent.ObserveBoard(1))
-                skipItems(3)
-
-                viewModel.onIntent(BoardIntent.StartCreatingCard(0, true))
-                awaitItem()
-
-                viewModel.onIntent(BoardIntent.CancelCardCreation)
-                val updatedState = awaitItem()
-                assertThat(updatedState.topBarType).isEqualTo(BoardAppBarType.DEFAULT)
-                assertThat(updatedState.cardCreationState).isEqualTo(CardCreationState())
-
-                cancelAndConsumeRemainingEvents()
-                coVerify { observeCompleteBoardUseCase(any()) }
-            }
-        }
-    }
-
-    @Test
-    fun `SHOULD update state WHEN new card title change`() {
-
-        coEvery { observeCompleteBoardUseCase(any()) } returns flowOf(
-            Result.Success(Board(id = 1, name = "test", emptyList()))
-        )
-
-        runTest(testDispatcher) {
-            viewModel.uiState.test {
-                viewModel.onIntent(BoardIntent.ObserveBoard(1))
-                skipItems(3)
-
-                viewModel.onIntent(BoardIntent.OnCardTitleChange("new title"))
-                assertThat(awaitItem().cardCreationState.title).isEqualTo("new title")
-
-                cancelAndConsumeRemainingEvents()
-                coVerify { observeCompleteBoardUseCase(any()) }
-            }
-        }
     }
 
     @Test
     fun `SHOULD send snackbar WHEN create card with null title`() {
+        val title = null
+        val position = column1.cards.size
+        val columnId = column1.id
 
-        val columnId = 1L
-        val initialBoard = Board(
-            id = 1,
-            name = "test",
-            columns = listOf(
-                KanbanColumn(
-                    id = columnId,
-                    name = "column",
-                    position = 0,
-                    color = null,
-                    cards = emptyList()
-                )
-            )
-        )
-
-        coEvery { observeCompleteBoardUseCase(any()) } returns flowOf(
-            Result.Success(initialBoard)
-        )
-
-        runTest(testDispatcher) {
-            viewModel.sideEffectFlow.test {
-                viewModel.onIntent(BoardIntent.ObserveBoard(1))
-                viewModel.onIntent(BoardIntent.StartCreatingCard(columnId, true))
+        testBoardViewModelSideEffect(
+            testSetUp = {
+                coEvery {
+                    createCardUseCase(title, position, columnId)
+                } returns CreateCardResult.EMPTY_TITLE
+            },
+            testBody = {
+                viewModel.onIntent(BoardIntent.StartCreatingCard(column1.id, true))
 
                 viewModel.onIntent(BoardIntent.ConfirmCardCreation)
                 assertThat(awaitItem()).isInstanceOf(BoardSideEffect.ShowSnackBar::class.java)
-
-                cancelAndConsumeRemainingEvents()
-                coVerify { observeCompleteBoardUseCase(any()) }
+            },
+            testValidate = {
+                coVerify { createCardUseCase(title, position, columnId) }
             }
-        }
+        )
     }
 
     @Test
     fun `SHOULD send snackbar WHEN create card with null columnId`() {
+        val newTitle = "new title"
+        val columnId = null
 
-        val columnId = 1L
-        val initialBoard = Board(
-            id = 1,
-            name = "test",
-            columns = listOf(
-                KanbanColumn(
-                    id = columnId,
-                    name = "column",
-                    position = 0,
-                    color = null,
-                    cards = emptyList()
-                )
-            )
-        )
-
-        coEvery { observeCompleteBoardUseCase(any()) } returns flowOf(
-            Result.Success(initialBoard)
-        )
-
-        runTest(testDispatcher) {
-            viewModel.sideEffectFlow.test {
-                viewModel.onIntent(BoardIntent.ObserveBoard(1))
+        testBoardViewModelSideEffect(
+            testSetUp = {
+                coEvery {
+                    createCardUseCase(newTitle, any(), columnId)
+                } returns CreateCardResult.GENERIC_ERROR
+            },
+            testBody = {
                 // columnId not specified because start create card is not called
-                viewModel.onIntent(BoardIntent.OnCardTitleChange("new title"))
+                viewModel.onIntent(BoardIntent.OnCardTitleChange(newTitle))
 
                 viewModel.onIntent(BoardIntent.ConfirmCardCreation)
                 assertThat(awaitItem()).isInstanceOf(BoardSideEffect.ShowSnackBar::class.java)
-
-                cancelAndConsumeRemainingEvents()
-                coVerify { observeCompleteBoardUseCase(any()) }
+            },
+            testValidate = {
+                coVerify { createCardUseCase(newTitle, any(), columnId) }
             }
-        }
+        )
     }
 
     @Test
     fun `SHOULD send snackbar WHEN did not find column while creating card`() {
+        val newTitle = "new title"
+        val columnId = -1L // column not found
 
-        val columnId = 1L
-        val initialBoard = Board(
-            id = 1,
-            name = "test",
-            columns = listOf(
-                KanbanColumn(
-                    id = columnId,
-                    name = "column",
-                    position = 0,
-                    color = null,
-                    cards = emptyList()
+        testBoardViewModelSideEffect(
+            testSetUp = {
+                coEvery {
+                    createCardUseCase(newTitle, any(), columnId)
+                } returns CreateCardResult.GENERIC_ERROR
+            },
+            testBody = {
+                viewModel.onIntent(
+                    BoardIntent.StartCreatingCard(
+                        columnId = columnId,
+                        isAppendingToEnd = true
+                    )
                 )
-            )
-        )
-
-        coEvery { observeCompleteBoardUseCase(any()) } returns flowOf(
-            Result.Success(initialBoard)
-        )
-
-        runTest(testDispatcher) {
-            viewModel.sideEffectFlow.test {
-                viewModel.onIntent(BoardIntent.ObserveBoard(1))
-                viewModel.onIntent(BoardIntent.OnCardTitleChange("new title"))
-                viewModel.onIntent(BoardIntent.StartCreatingCard(columnId + 1, true)) // Should not find any column
+                viewModel.onIntent(BoardIntent.OnCardTitleChange(newTitle))
 
                 viewModel.onIntent(BoardIntent.ConfirmCardCreation)
                 assertThat(awaitItem()).isInstanceOf(BoardSideEffect.ShowSnackBar::class.java)
-
-                cancelAndConsumeRemainingEvents()
-                coVerify { observeCompleteBoardUseCase(any()) }
+            },
+            testValidate = {
+                coVerify { createCardUseCase(newTitle, any(), columnId) }
             }
-        }
+        )
     }
 
     @Test
-    fun `SHOULD call card repository WHEN create card`() {
+    fun `SHOULD call card repository WHEN create card`() = runTest(testDispatcher) {
+        val newTitle = "new title"
+        val position = column1.cards.size
+        val columnId = column1.id
 
-        val columnId = 1L
-        val initialBoard = Board(
-            id = 1,
-            name = "test",
-            columns = listOf(
-                KanbanColumn(
-                    id = columnId,
-                    name = "column",
-                    position = 0,
-                    color = null,
-                    cards = emptyList()
-                )
-            )
+        coEvery { observeCompleteBoardUseCase(defaultBoard.id) } returns flowOf(
+            Result.Success(defaultBoard)
         )
+        coEvery { createCardUseCase(newTitle, position, columnId) } returns CreateCardResult.SUCCESS
 
-        coEvery { observeCompleteBoardUseCase(any()) } returns flowOf(
-            Result.Success(initialBoard)
-        )
+        viewModel.onIntent(BoardIntent.ObserveBoard(defaultBoard.id))
+        viewModel.onIntent(BoardIntent.StartCreatingCard(columnId, true))
+        viewModel.onIntent(BoardIntent.OnCardTitleChange(newTitle))
+        viewModel.onIntent(BoardIntent.ConfirmCardCreation)
 
-        coEvery { createCardUseCase(any(), any(), columnId) } returns CreateCardResult.SUCCESS
-
-        runTest(testDispatcher) {
-
-            viewModel.onIntent(BoardIntent.ObserveBoard(1))
-            viewModel.onIntent(BoardIntent.StartCreatingCard(columnId, true))
-            viewModel.onIntent(BoardIntent.OnCardTitleChange("new title"))
-            viewModel.onIntent(BoardIntent.ConfirmCardCreation)
-
-            coVerify {
-                observeCompleteBoardUseCase(any())
-                createCardUseCase(any(), any(), columnId)
-            }
-        }
-    }
-
-    @Test
-    fun `SHOULD send snackbar WHEN create card with result error`() {
-
-        val columnId = 1L
-        val initialBoard = Board(
-            id = 1,
-            name = "test",
-            columns = listOf(
-                KanbanColumn(
-                    id = columnId,
-                    name = "column",
-                    position = 0,
-                    color = null,
-                    cards = emptyList()
-                )
-            )
-        )
-
-        coEvery { observeCompleteBoardUseCase(any()) } returns flowOf(
-            Result.Success(initialBoard)
-        )
-        coEvery { createCardUseCase(any(), any(), columnId) } returns CreateCardResult.GENERIC_ERROR
-
-        runTest(testDispatcher) {
-            viewModel.sideEffectFlow.test {
-                viewModel.onIntent(BoardIntent.ObserveBoard(1))
-                viewModel.onIntent(BoardIntent.StartCreatingCard(columnId, true))
-                viewModel.onIntent(BoardIntent.OnCardTitleChange("new title"))
-
-                viewModel.onIntent(BoardIntent.ConfirmCardCreation)
-                assertThat(awaitItem()).isInstanceOf(BoardSideEffect.ShowSnackBar::class.java)
-
-                cancelAndConsumeRemainingEvents()
-                coVerify {
-                    observeCompleteBoardUseCase(any())
-                    createCardUseCase(any(), any(), columnId)
-                }
-            }
+        coVerify {
+            createCardUseCase(newTitle, position, columnId)
         }
     }
 
---
+++ app/src/test/java/com/educost/kanone/presentation/screens/board/BoardViewModelTest.kt
@@ -0,0 +1,133 @@
+package com.educost.kanone.presentation.screens.board
+
+import app.cash.turbine.TurbineTestContext
+import app.cash.turbine.test
+import com.educost.kanone.dispatchers.DispatcherProvider
+import com.educost.kanone.dispatchers.TestDispatcherProvider
+import com.educost.kanone.domain.model.Board
+import com.educost.kanone.domain.model.KanbanColumn
+import com.educost.kanone.domain.usecase.CreateCardUseCase
+import com.educost.kanone.domain.usecase.ObserveCompleteBoardUseCase
+import com.educost.kanone.presentation.screens.board.state.BoardState
+import com.educost.kanone.utils.Result
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+
+@Suppress("UnusedFlow")
+open class BoardViewModelTest {
+
+    protected lateinit var testDispatcher: CoroutineDispatcher
+    protected lateinit var dispatcherProvider: DispatcherProvider
+
+    protected lateinit var observeCompleteBoardUseCase: ObserveCompleteBoardUseCase
+    protected lateinit var createCardUseCase: CreateCardUseCase
+
+    protected lateinit var viewModel: BoardViewModel
+
+
+    protected val column1 = KanbanColumn(
+        id = 1,
+        name = "column1",
+        position = 1
+    )
+    protected val column2 = KanbanColumn(
+        id = 2,
+        name = "column2",
+        position = 2
+    )
+
+    protected val defaultBoard = Board(
+        id = 1,
+        name = "test",
+        columns = listOf(column1, column2)
+    )
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Before
+    fun setUp() {
+        testDispatcher = UnconfinedTestDispatcher()
+        dispatcherProvider = TestDispatcherProvider(testDispatcher)
+        observeCompleteBoardUseCase = mockk()
+        createCardUseCase = mockk()
+
+        viewModel = BoardViewModel(
+            dispatcherProvider = dispatcherProvider,
+            observeCompleteBoardUseCase = observeCompleteBoardUseCase,
+            createColumnUseCase = mockk(),
+            createCardUseCase = createCardUseCase,
+            updateColumnUseCase = mockk(),
+            deleteColumnUseCase = mockk(),
+            restoreColumnUseCase = mockk(),
+            persistBoardPositionsUseCase = mockk(),
+            reorderCardsUseCase = mockk(),
+            updateBoardUseCase = mockk(),
+            deleteBoardUseCase = mockk()
+        )
+    }
+
+    fun testBoardViewModelUiState(
+        testDispatcher: CoroutineDispatcher = this.testDispatcher,
+        board: Board = defaultBoard,
+        testBody: suspend TurbineTestContext<BoardState>.() -> Unit
+    ) = runTest(testDispatcher) {
+
+        // SETUP
+        coEvery {
+            observeCompleteBoardUseCase(board.id)
+        } returns flowOf(Result.Success(board))
+
+        viewModel.uiState.test {
+            viewModel.onIntent(BoardIntent.ObserveBoard(board.id))
+
+            skipItems(3) // Loading state = true  ->  New Board  ->  Loading state = false
+
+
+            // TEST
+            testBody()
+
+
+            // VALIDATE
+            cancelAndConsumeRemainingEvents()
+            coVerify { observeCompleteBoardUseCase(board.id) }
+        }
+    }
+
+    fun testBoardViewModelSideEffect(
+        testDispatcher: CoroutineDispatcher = this.testDispatcher,
+        board: Board = defaultBoard,
+        testSetUp: TestScope.() -> Unit = {},
+        testBody: suspend TurbineTestContext<BoardSideEffect>.() -> Unit,
+        testValidate: TestScope.() -> Unit = {}
+    ) = runTest(testDispatcher) {
+
+        // SETUP
+        coEvery {
+            observeCompleteBoardUseCase(board.id)
+        } returns flowOf(Result.Success(board))
+
+        testSetUp()
+
+        viewModel.sideEffectFlow.test {
+            viewModel.onIntent(BoardIntent.ObserveBoard(board.id))
+
+
+            // TEST
+            testBody()
+
+
+            // VALIDATE
+            cancelAndConsumeRemainingEvents()
+            testValidate()
+            coVerify { observeCompleteBoardUseCase(board.id) }
+        }
+    }
+
+}
