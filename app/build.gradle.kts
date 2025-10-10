d977195fafd47ea64c3d038002ccf7b4be5cbbe7
EduCost <edu.oliveira03c@gmail.com>
2025-10-10 14:06:30 -0300

    Updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.educost.kanone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.0-alpha"
+        versionCode = 3
+        versionName = "0.2.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
