1e41e15a041fb93dcd77a7aa44f45031005f3877
EduCost <edu.oliveira03c@gmail.com>
2025-12-14 00:31:38 -0300

    Introduced `LocalThemeData` CompositionLocal to provide theme data down the tree

--- app/src/main/java/com/educost/kanone/presentation/navigation/AppEntry.kt
+++ app/src/main/java/com/educost/kanone/presentation/navigation/AppEntry.kt
@@ -1,6 +1,8 @@
 package com.educost.kanone.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -8,6 +10,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.educost.kanone.presentation.theme.KanOneTheme
+import com.educost.kanone.presentation.theme.ThemeData
+
+val LocalThemeData = compositionLocalOf { ThemeData() }
 
 @Composable
 fun AppEntry(
@@ -19,16 +24,16 @@ fun AppEntry(
     val themeData by viewModel.themeData.collectAsStateWithLifecycle()
 
     themeData?.let { themeData ->
-        KanOneTheme(
-            themeData = themeData
-        ) {
-            NavHost(
-                modifier = modifier,
-                navController = navController,
-                startDestination = MainDestinations
-            ) {
-                mainNavGraph(navController)
-                settingsNavGraph(navController)
+        CompositionLocalProvider(LocalThemeData provides themeData) {
+            KanOneTheme(themeData = themeData) {
+                NavHost(
+                    modifier = modifier,
+                    navController = navController,
+                    startDestination = MainDestinations
+                ) {
+                    mainNavGraph(navController)
+                    settingsNavGraph(navController)
+                }
             }
         }
     }
